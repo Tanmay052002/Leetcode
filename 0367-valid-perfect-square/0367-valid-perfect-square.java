d020c1851dfb21ea6ffdbb22cc1f60df5ffc86ef
Tanmay052002 <106682701+Tanmay052002@users.noreply.github.com>
2024-07-03 19:02:31 +0530

    Time: 0 ms (100%), Space: 40 MB (70.67%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int x) {
+        long low = 1;
+        long high = x;
+
+        while(low <= high){
+            long mid = low + (high-low)/2;
+            if(mid*mid == x){
+                return true;
+            }else if(mid*mid > x){
+                high = mid-1;
+            }else if(mid*mid < x){
+                low = mid+1;
+            }
+        }
+        
+        return false;
+    }
+}
