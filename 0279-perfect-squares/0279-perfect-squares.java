9e97ecb175491bc6f9676271da50216c7aa59afe
Tanmay052002 <106682701+Tanmay052002@users.noreply.github.com>
2024-07-18 16:06:33 +0530

    Time: 63 ms (33.38%), Space: 42.9 MB (40.41%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSquares(int n) {
+      int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        int res = numHelper(n,dp);
+
+        return res;
+    }
+    public int numHelper(int n,int dp[]){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int smallest = Integer.MAX_VALUE;
+        for(int i = 1; i*i<=n ; i++){
+
+            int temp = numHelper(n-i*i,dp);
+
+            smallest = Math.min(smallest,temp);
+        }
+
+        dp[n] = smallest+1 ;
+
+        return smallest+1;  
+    }
+}
