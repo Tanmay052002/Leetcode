3f6ec40dabc23c6f47aa7786f9c3523a37e35c5e
Tanmay052002 <106682701+Tanmay052002@users.noreply.github.com>
2024-10-05 01:08:55 +0530

    Time: 1376 ms (32.62%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT P.product_name , S.year, S.price
 FROM Sales S 
-JOIN Product P 
-ON P.product_id  = S.product_id ;
+NATURAL JOIN Product P 
+-- ON P.product_id  = S.product_id ;
